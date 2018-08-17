d6302b48c8ae8a1c6318cb20b5befb741baf99a4
Sen Mu <190567268@qq.com>
2018-08-17 15:24:38 +0800

    initial commit
    get danmaku directly

---
+++ getdanmako.py
@@ -0,0 +1,66 @@
+'''
+Author: Komoriii
+email: c2VuZXZhbkBmb3htYWlsLmNvbQ==
+'''
+import requests
+import random
+from bs4 import BeautifulSoup
+
+
+# bilibili danmaku api "https://comment.bilibili.com/xxxxxxxx.xml"
+DANMAKU_API = "https://comment.bilibili.com/{0}.xml"
+UA_list = []
+cids = {}
+danmaku_list = []
+random.seed()
+
+class Danmaku_pool():
+    def __init__(self):
+        self.pool = []
+    
+    def append_dm(self, dm_list):
+        self.pool.append(dm_list)
+
+    def print(self):
+        print(self.pool)
+    
+    def save_to_txt(self, filename):
+        f = open(filename, 'w')
+        for i in self.pool:
+            for line in i:
+                # change to utf encoding, otherwise this cause an error
+                f.writelines(line)
+        f.close()
+
+# read all of the User-Agents
+def read_lines(filename):
+    list_temp = []
+    f = open(filename, 'r')
+    for line in f:
+        list_temp.append(line)
+    return list_temp
+
+def get_danmaku1v(user_agent, cid):
+    headers = {
+        'User-Agent': user_agent,
+    }
+    r = requests.get(DANMAKU_API.format(cid))
+    raw_file = r.content
+    danmaku_1v = []
+    formated_file = BeautifulSoup(raw_file, "lxml")
+    for dm in formated_file.find_all('d'):
+        danmaku_1v.append(dm.get_text())
+    return danmaku_1v
+
+
+def main():
+    danmaku_pool = Danmaku_pool()
+    UA_list = read_lines('UAs.txt')
+    user_agent = UA_list[random.randrange(len(UA_list))]
+    cids = set(read_lines('cids.txt'))
+    for c in cids:
+        danmaku_pool.append_dm(get_danmaku1v(user_agent, c))
+    #danmaku_pool.save_to_txt('danmaku_sv.txt')
+
+if __name__ == '__main__':
+    main()
